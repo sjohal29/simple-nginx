1de07a0af802fdbd650a8e7b32f664eadc79a14d
Aaron Rueth <arueth@gmail.com>
2019-10-25 09:51:07 -0700

    Cleaning up entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/ash
-if [ /run/secrets/mysecret != null ]
-then
-ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
-else
-ln -s /run/secrets/mysecret/ /usr/share/nginx/html/mysecret.html
-fi
-# ln -s /run/secrets/mysecret/mysecret /usr/share/nginx/html/mysecret.html
+
+ln -s /var/run/secrets/mysecret /usr/share/nginx/html/mysecret.html
+
 exec "$@"
