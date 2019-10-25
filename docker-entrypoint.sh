274eb81f75d45ea5ef2a93d8a1dd060582ba6a53
Marlon Ruiz <43421777+ruizmarlon@users.noreply.github.com>
2019-10-25 00:08:53 -0400

    update docker-entrypoint to support k8s with if check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/ash
+if [ /run/secrets/mysecret != null ]
+then
 ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
+else
+ln -s /run/secrets/mysecret/mysecret /usr/share/nginx/html/mysecret.html
+fi
+# ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
 exec "$@"
