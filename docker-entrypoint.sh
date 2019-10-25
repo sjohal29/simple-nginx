f89d47608c0a7ba7d095b65aecd8c1bebe9ea9dd
Marlon Ruiz <43421777+ruizmarlon@users.noreply.github.com>
2019-10-25 01:23:33 -0400

    added mysecrets for kube

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ then
 ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
 else
 #ln -s /run/secrets/mysecret/ /usr/share/nginx/html/mysecret.html
-ls -s /etc/secrets-volume /usr/share/nginx/html/mysecret.html
+ls -s /etc/secrets-volume/mysecret /usr/share/nginx/html/mysecret.html
 fi
 # ln -s /run/secrets/mysecret/mysecret /usr/share/nginx/html/mysecret.html
 exec "$@"
