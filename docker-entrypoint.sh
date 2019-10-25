72a99b5f7079ad9262224f41fd2f987683b234a1
Marlon Ruiz <43421777+ruizmarlon@users.noreply.github.com>
2019-10-25 00:51:25 -0400

    update docker-entrypoint.sh changed ls -> cp for kube secret

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,8 @@ if [ /run/secrets/mysecret != null ]
 then
 ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
 else
-ln -s /run/secrets/mysecret/mysecret /usr/share/nginx/html/mysecret.html
+#ln -s /run/secrets/mysecret/mysecret /usr/share/nginx/html/mysecret.html
+cp /run/secrets/mysecret/mysecret /usr/share/nginx/html/mysecret.html
 fi
 # ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
 exec "$@"
