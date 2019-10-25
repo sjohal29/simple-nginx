941b23d56d0f8d34dae418b1490718e4a4a9e1e7
Marlon Ruiz <43421777+ruizmarlon@users.noreply.github.com>
2019-10-25 01:17:36 -0400

    update docker-entrypoint.sh change to location /etc/secrets-volume

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,8 @@ if [ /run/secrets/mysecret != null ]
 then
 ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
 else
-ln -s /run/secrets/mysecret/ /usr/share/nginx/html/mysecret.html
+#ln -s /run/secrets/mysecret/ /usr/share/nginx/html/mysecret.html
+ls -s /etc/secrets-volume /usr/share/nginx/html/mysecret.html
 fi
 # ln -s /run/secrets/mysecret/mysecret /usr/share/nginx/html/mysecret.html
 exec "$@"
