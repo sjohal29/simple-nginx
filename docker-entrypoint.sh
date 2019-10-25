b485e11547e69ec1926fa0eda9357228c002d9ee
Marlon Ruiz <43421777+ruizmarlon@users.noreply.github.com>
2019-10-25 01:12:55 -0400

    update docker-entrypoint.sh 2 mysecrets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,5 @@ ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
 else
 ln -s /run/secrets/mysecret/ /usr/share/nginx/html/mysecret.html
 fi
-# ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
+# ln -s /run/secrets/mysecret/mysecret /usr/share/nginx/html/mysecret.html
 exec "$@"
