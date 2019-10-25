f2d853e3831a9bd5fa5e8cf64a8dee156d8e7c3c
Marlon Ruiz <43421777+ruizmarlon@users.noreply.github.com>
2019-10-25 01:09:39 -0400

    update docker-entrypoint.sh only 1 mysecret

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ if [ /run/secrets/mysecret != null ]
 then
 ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
 else
-ln -s /run/secrets/mysecret/mysecret/mysecret /usr/share/nginx/html/mysecret.html
+ln -s /run/secrets/mysecret/ /usr/share/nginx/html/mysecret.html
 fi
 # ln -s /run/secrets/mysecret /usr/share/nginx/html/mysecret.html
 exec "$@"
