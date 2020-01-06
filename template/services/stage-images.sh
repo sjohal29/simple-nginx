fbaf8ef6ec5b78ce72296401906b146bfde1f23a
Matt Bentley <matt.bentley@docker.com>
2020-01-06 14:01:04 -0500

    Updated with my settings
    Signed-off-by: Matt Bentley <matt.bentley@docker.com>

--- template/services/stage-images.sh
+++ template/services/stage-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build ./front-end -f ./front-end/Dockerfile.linux -t ${REGISTRY_FQDN}/app-template/simple-nginx-front-end:latest
-docker build ./db -f ./db/Dockerfile.linux -t ${REGISTRY_FQDN}/app-template/simple-nginx-db:latest
+docker build ./front-end -f ./front-end/Dockerfile.linux -t ${REGISTRY_FQDN}/matt.bentley/simple-nginx-front-end:latest
+docker build ./db -f ./db/Dockerfile.linux -t ${REGISTRY_FQDN}/matt.bentley/simple-nginx-db:latest
